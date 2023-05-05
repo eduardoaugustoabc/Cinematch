f2b95cffbef9449c3b9a1266413ae9dcaa5de2a6
email <lucas.lima.silva@ccc.ufcg.edu.br>
2023-05-04 22:00:56 -0300

    Removi coisas desnecessárias do RepositorioFilme e criei um get

--- src/RepositorioFilmes.hs
+++ src/RepositorioFilmes.hs
@@ -2,26 +2,21 @@ module RepositorioFilmes where
 
 import Filme
 
+{-Tipo Repositorio de Filmes. Guarda um array de filmes.-}
+
 data RepositorioFilmes = RepositorioFilmes {
-    filmes :: [Filme]
+  filmes :: [Filme]
 } deriving (Show)
 
+{-Chama o construtor de Filme e insere os dados-}
 criarFilme :: String -> [String] -> String -> String-> [String] -> String -> String -> Float -> String -> String -> Int -> Filme
 criarFilme titulo generos descricao diretor atores dataLancamento duracao notaUsuarios usuariosVotates arrecadacao notaImdb =
   Filme { titulo = titulo, generos = generos, descricao = descricao, diretor = diretor, atores = atores, dataLancamento = dataLancamento, duracao = duracao, notaUsuarios = notaUsuarios, usuariosVotates = usuariosVotates, arrecadacao = arrecadacao, notaImdb = notaImdb, notaUsuario = 0.0 }
 
-getFilmes :: RepositorioFilmes -> [Filme]
-getFilmes (RepositorioFilmes { filmes = fs }) = fs
+{-Get do repositório, retorna a lista de filmes-}
+getRepFilmes :: RepositorioFilmes -> [Filme]
+getRepFilmes (RepositorioFilmes fs) = fs
 
-getFilme :: String -> RepositorioFilmes -> String
-getFilme nomeFilme repoFilmes =
-  let
-    filmeEncontrado = head $ filter (\f -> titulo f == nomeFilme) (filmes repoFilmes)
-  in
-    show filmeEncontrado
-
+{-Adiciona filme no repositório. Recebe um repositório e o filme a ser adicionado, concatena o array de filmes com o novo filme dentro do repositório e retorna o resltado que é um RepositorioFilmes-}
 addFilmeRepositorio :: RepositorioFilmes -> Filme -> RepositorioFilmes
 addFilmeRepositorio repositorio novoFilme = RepositorioFilmes {filmes = filmes repositorio ++ [novoFilme]}
-
-addFilmee :: RepositorioFilmes -> Filme -> RepositorioFilmes
-addFilmee repositorio novoFilme = repositorio { filmes = (filmes repositorio ++ [novoFilme]) }
