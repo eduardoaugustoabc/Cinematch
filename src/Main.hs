860280f63b33ee356123c8bdca0e22f039233525
email <lucas.lima.silva@ccc.ufcg.edu.br>
2023-05-04 22:13:10 -0300

    O mais importante desta mudança é que Usuario também fará parte da função acao. O menu de interação ainda está engessado, logo mais será otimizado.

--- src/Main.hs
+++ src/Main.hs
@@ -1,30 +1,58 @@
 import Filme
 import RepositorioFilmes
+import Usuario
 
 main :: IO ()
 main = do
-  print "\nAções:\n[A]: Cadastrar Filme\n[B]: Exibir Filme\n[S]: Sair"
+  print "\nAções:\n[A]: Cadastrar Filme\n[B]: Exibir Filme\n[S]: Sair\n[F]: favoritar ator\n[O]: favoritar diretor\n[I]: favoritar genero\n [W]: Recomendacao"
   comando <- readLn :: IO Char
-  repositorio <- acoes (RepositorioFilmes { filmes = [] }) comando
-  --repositorio <- addFilme repositorio
-  --repositorio <- addFilme repositorio
-  print repositorio
+  repositorio <- acoes (RepositorioFilmes { filmes = [] }) (Usuario { generosFav = ["ação", "aventura"]
+                       , diretoresFav = ["Tarantino"]
+                       , atoresFav = ["Keanu Reeves", "Scarlett Johansson"]
+                       , filmesFav = []
+                       , watchlist = []
+                       , filmesAssistidos = []
+                       }) comando
+  print "\nFim"
 
-acoes :: RepositorioFilmes -> Char -> IO RepositorioFilmes
-acoes rep cmd
+acoes :: RepositorioFilmes -> Usuario -> Char -> IO RepositorioFilmes
+acoes rep us cmd
     | cmd == 'A' = do
         repo <- addFilme rep
         comando <- readLn :: IO Char
-        acoes repo comando
+        acoes repo us comando
     | cmd == 'B' = do
         exibirFilme rep
         comando <- readLn :: IO Char
-        acoes rep comando
+        acoes rep us comando
+    | cmd == 'F' = do
+        com <- readLn :: IO String
+        us <- (favoritarAtor us com)
+        print (getAtores us)
+        comando <- readLn :: IO Char
+        acoes rep us comando
+    | cmd == 'I' = do
+        com <- readLn :: IO String
+        us <- (favoritarGenenero us com)
+        print (getGeneros us)
+        comando <- readLn :: IO Char
+        acoes rep us comando
+    |cmd == 'O' = do
+        com <- readLn :: IO String
+        us <- (favoritarDiretor us com)
+        print (getDiretores us)
+        comando <- readLn :: IO Char
+        acoes rep us comando
+    | cmd == 'W' = do
+      qtd <- readLn :: IO Int
+      print(recomenda qtd (getFilmesFav us) (getRepFilmes rep) us)
+      comando <- readLn :: IO Char
+      acoes rep us comando
     | cmd == 'S' = return rep
     | otherwise = do
         putStrLn "Comando inválido"
         comando <- readLn :: IO Char
-        acoes rep comando
+        acoes rep us comando
 
 exibirFilme :: RepositorioFilmes -> IO ()
 exibirFilme rep = print rep
