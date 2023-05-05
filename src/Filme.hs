671da17ceaa62b492c8d3de6e07f984f4ba4f01f
email <lucas.lima.silva@ccc.ufcg.edu.br>
2023-05-04 21:52:24 -0300

    Adicionei gets a filme.

--- src/Filme.hs
+++ src/Filme.hs
@@ -1,19 +1,23 @@
 module Filme where
 
+{-Tipo Filme. tem todos os itens necessarios de um filme.-}
+
 data Filme = Filme {
-    titulo :: String,
-    generos :: [String],
-    descricao :: String,
-    diretor :: String,
-    atores :: [String],
-    dataLancamento :: String,
-    duracao :: String,
-    notaUsuarios :: Float,
-    usuariosVotates :: String,
-    arrecadacao :: String,
-    notaImdb :: Int,
-    notaUsuario :: Float
-}
+  titulo :: String,
+  generos :: [String],
+  descricao :: String,
+  diretor :: String,
+  atores :: [String],
+  dataLancamento :: String,
+  duracao :: String,
+  notaUsuarios :: Float,
+  usuariosVotates :: String,
+  arrecadacao :: String,
+  notaImdb :: Int,
+  notaUsuario :: Float
+  }
+
+{-Representação textual do Filme.-}
 
 instance Show Filme where
   show f = "\n-----------------------------" ++
@@ -30,3 +34,25 @@ instance Show Filme where
            "\nNota do IMDB: " ++ show (notaImdb f) ++
            "\nNota do usuário: " ++ show (notaUsuario f) ++
            "\n-----------------------------"
+
+{-Gets de dados de um filme-}
+getGenerosFilme :: Filme -> [String]
+getGenerosFilme (Filme { generos = gn}) = gn
+
+getDiretorFilme :: Filme -> String
+getDiretorFilme (Filme { diretor = dr}) = dr
+
+getAtoresFilme :: Filme -> [String]
+getAtoresFilme (Filme { atores = at}) = at
+
+getTituloFilme :: Filme -> String
+getTituloFilme (Filme { titulo = t}) = t
+
+getDataFilme :: Filme -> String
+getDataFilme (Filme { dataLancamento = d}) = d
+
+getNotaImdbFilme :: Filme -> Int
+getNotaImdbFilme (Filme { notaImdb = nI }) = nI
+
+getNotaUsuariosFilme :: Filme -> Float
+getNotaUsuariosFilme (Filme { notaUsuarios = nU }) = nU
