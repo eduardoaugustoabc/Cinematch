c06b7c5d171c5e60422cfa6d9010864b8392dad1
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-05-05 22:40:51 -0300

    main , opcoes , refatoração de ações , leitura do arquivo .csv , salvar filmes lidos no csv para o repositorio , split

--- src/Main.hs
+++ src/Main.hs
@@ -1,58 +1,89 @@
 import Filme
 import RepositorioFilmes
 import Usuario
+import Text.CSV
 
 main :: IO ()
 main = do
-  print "\nAções:\n[A]: Cadastrar Filme\n[B]: Exibir Filme\n[S]: Sair\n[F]: favoritar ator\n[O]: favoritar diretor\n[I]: favoritar genero\n [W]: Recomendacao"
-  comando <- readLn :: IO Char
-  repositorio <- acoes (RepositorioFilmes { filmes = [] }) (Usuario { generosFav = ["ação", "aventura"]
+  putStrLn "---------------------------------------------------------------"
+  putStrLn "                 Bem vindo(a) ao CINEMATCH                     "
+  putStrLn "---------------------------------------------------------------"
+  let arquivo = "IMDB-Movie-Data.csv"
+  resultado <- parseCSVFromFile arquivo
+  case resultado of
+    Right linhas -> do
+      addAll linhas (RepositorioFilmes { filmes = [] }) (Usuario { generosFav = ["ação", "aventura"]
                        , diretoresFav = ["Tarantino"]
                        , atoresFav = ["Keanu Reeves", "Scarlett Johansson"]
                        , filmesFav = []
                        , watchlist = []
                        , filmesAssistidos = []
-                       }) comando
-  print "\nFim"
+                       })
+  putStrLn "Obrigado por usar o CINEMATCH!"
 
-acoes :: RepositorioFilmes -> Usuario -> Char -> IO RepositorioFilmes
-acoes rep us cmd
-    | cmd == 'A' = do
-        repo <- addFilme rep
-        comando <- readLn :: IO Char
-        acoes repo us comando
-    | cmd == 'B' = do
-        exibirFilme rep
-        comando <- readLn :: IO Char
-        acoes rep us comando
-    | cmd == 'F' = do
-        com <- readLn :: IO String
-        us <- (favoritarAtor us com)
-        print (getAtores us)
-        comando <- readLn :: IO Char
-        acoes rep us comando
-    | cmd == 'I' = do
-        com <- readLn :: IO String
-        us <- (favoritarGenenero us com)
-        print (getGeneros us)
-        comando <- readLn :: IO Char
-        acoes rep us comando
-    |cmd == 'O' = do
-        com <- readLn :: IO String
-        us <- (favoritarDiretor us com)
-        print (getDiretores us)
-        comando <- readLn :: IO Char
-        acoes rep us comando
-    | cmd == 'W' = do
-      qtd <- readLn :: IO Int
-      print(recomenda qtd (getFilmesFav us) (getRepFilmes rep) us)
-      comando <- readLn :: IO Char
-      acoes rep us comando
-    | cmd == 'S' = return rep
-    | otherwise = do
-        putStrLn "Comando inválido"
-        comando <- readLn :: IO Char
-        acoes rep us comando
+
+opcoes :: RepositorioFilmes -> Usuario -> IO ()
+opcoes rep user = do
+  putStrLn "++---------------------Escolha uma ação----------------------++"
+  putStrLn " 1) Exibir Filme"
+  putStrLn " 2) Favoritar Gênero"
+  putStrLn " 3) Favoritar Ator"
+  putStrLn " 4) Favoritar Diretor"
+  putStrLn " 5) Recomendação"
+  putStrLn " 6) Lista de diretores favoritos"
+  putStrLn " 7) Lista de filmes que se deseja assistir"
+  putStrLn " 8) Cadastrar Filme"
+  putStrLn " 9) Dashboard"
+  putStrLn " 10) EXIT PROGRAM"
+  putStr "Opção escolhida : "
+  opcao <- getLine
+  putStrLn "++-----------------------------------------------------------++"
+  if opcao == "10"
+    then return ()
+  else do
+    novoRepo <- acoes opcao rep user
+    return ()
+
+acoes :: String -> RepositorioFilmes -> Usuario -> IO ()
+acoes cmd rep user
+  | cmd == "1"     =  do
+    exibirFilme rep
+    opcoes rep user
+  | cmd == "2"     = do
+    putStr "Qual gênero deseja favoritar : "
+    com <- getLine
+    us <- (favoritarGenenero user com)
+    print (getGeneros us)
+    opcoes rep us
+  | cmd == "3"     = do
+    putStr "Qual ator deseja favoritar : "
+    com <- getLine
+    us <- (favoritarAtor user com)
+    print (getAtores us)
+    opcoes rep us
+  | cmd == "4"     = do
+    putStr "Qual diretor deseja favoritar : "
+    com <- getLine
+    us <- (favoritarDiretor user com)
+    print (getDiretores us)
+    opcoes rep us
+  | cmd == "5"     = do
+    qtd <- readLn :: IO Int
+    print(recomenda qtd (getFilmesFav user) (getRepFilmes rep) user)
+    opcoes rep user
+  | cmd == "8"     = do
+    repo <- addFilme rep
+    opcoes repo user
+  | cmd == "10"    = do
+    return ()
+  | otherwise      = do
+    putStrLn "Comando inválido ou não implementado até o momento"
+
+
+
+
+
+
 
 exibirFilme :: RepositorioFilmes -> IO ()
 exibirFilme rep = print rep
@@ -64,13 +95,45 @@ addFilme rep = do
 
 lerCriaFilme :: IO Filme
 lerCriaFilme = do
-  titulo <- readLn :: IO String
-  generos <- readLn :: IO [String]
-  descricao <- readLn :: IO String
-  diretor <- readLn :: IO String
-  atores <- readLn :: IO [String]
-  dataLancamento <- readLn :: IO String
-  duracao <- readLn :: IO String
-  notaImdb <- readLn :: IO Int
-  let filme = criarFilme titulo generos descricao diretor atores dataLancamento duracao notaImdb
+  putStrLn "Digite o nome do filme:"
+  titulo <- getLine
+  putStrLn "Digite os gêneros do filme (separados por espaço):"
+  generos <- getLine
+  putStrLn "Digite a descrição do filme:"
+  descricao <- getLine
+  putStrLn "Digite o nome do diretor do filme:"
+  diretor <- getLine
+  putStrLn "Digite os nomes dos atores do filme (separados por espaço):"
+  atores <- getLine
+  putStrLn "Digite a data de lançamento do filme (formato: dd/mm/aaaa):"
+  dataLancamento <- getLine
+  putStrLn "Digite a duração do filme (em minutos):"
+  duracao <- getLine
+  putStrLn "Digite a nota do filme no IMDb (entre 0 e 100):"
+  notaImdb <- getLine
+  let filme = criarFilme titulo (split ',' generos) descricao diretor (split ',' atores) dataLancamento duracao (read notaImdb :: Int)
   return filme
+
+
+addAll :: [[String]] -> RepositorioFilmes -> Usuario -> IO ()
+addAll [] rep user = opcoes rep user
+addAll (x:xs) rep user = do
+  let titulo = x !! 1 
+      generos = x !! 2
+      descricao = x !! 3 
+      diretor = x !! 4
+      atores = x !! 5
+      dataLancamento = x !! 6
+      duracao = x !! 7
+      notaImdb = x !! 11
+      filme = criarFilme titulo (split ',' generos) descricao diretor (split ',' atores) dataLancamento duracao (read notaImdb :: Int)
+  addAll xs (addFilmeRepositorio rep filme) user
+
+split :: Char -> String -> [String]
+split separador str = separateBy separador str "" []
+
+separateBy :: Char -> String -> String -> [String] -> [String]
+separateBy _ [] strAux lista = lista ++ [strAux]
+separateBy separador (x : xs) strAux lista
+  | x /= separador = separateBy separador xs (strAux ++ [x]) lista
+  | otherwise = separateBy separador xs "" (lista ++ [strAux])
