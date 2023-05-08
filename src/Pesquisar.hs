834adfb4f794e3eb3c898112d1551f92389fa4f2
arthursoares <arthursoares.js@gmail>
2023-05-08 17:10:32 -0300

    Adição de pesquisar por ator ou por diretor

--- src/Pesquisar.hs
+++ src/Pesquisar.hs
@@ -27,12 +27,28 @@ pesquisaPorAno filmes ano =
   where
     mesmoAno ano filme = ano == (getDataFilme filme)
 
+pesquisaPorDiretor :: [Filme] -> String -> Maybe [Filme]
+pesquisaPorDiretor filmes diretor = 
+  case filter (mesmoDiretor diretor) filmes of
+    []    -> Nothing
+    lista -> Just lista
+  where
+    mesmoDiretor diretor filme = diretor == (getDiretorFilme filme)
+
+pesquisaPorAtor :: [Filme] -> String -> Maybe [Filme]
+pesquisaPorAtor filmes ator =
+  case filter (mesmoAtor ator) filmes of
+    []    -> Nothing
+    lista -> Just lista
+  where
+    mesmoAtor ator filme = containsString ator (getAtoresFilme filme)
+
 containsString :: String -> [String] -> Bool
 containsString str list = elem str list
 
 selecaoPesquisa::RepositorioFilmes -> IO()
 selecaoPesquisa rep = do
-  putStrLn "Digite o número de acordo com a pesquisa que deseja fazer:\n1)Nome 2)Gênero 3)Ano"
+  putStrLn "Digite o número de acordo com a pesquisa que deseja fazer:\n1)Nome 2)Gênero 3)Ano 4)Diretor 5) Ator"
   cmd <- getLine
   let filmes = getRepFilmes rep
   case cmd of
@@ -54,6 +70,20 @@ selecaoPesquisa rep = do
       putStrLn "Digite o ano:"
       ano <- getLine
       let resultado = pesquisaPorAno filmes ano
+      case resultado of
+        Nothing -> putStrLn "Nenhum filme encontrado."
+        Just filmes -> mapM_ print filmes
+    "4" -> do
+      putStrLn "Digite o Diretor:"
+      diretor <- getLine
+      let resultado = pesquisaPorDiretor filmes diretor
+      case resultado of
+        Nothing -> putStrLn "Nenhum filme encontrado."
+        Just filmes -> mapM_ print filmes
+    "5" -> do
+      putStrLn "Digite o Ator:"
+      ator <- getLine
+      let resultado = pesquisaPorAtor filmes ator
       case resultado of
         Nothing -> putStrLn "Nenhum filme encontrado."
         Just filmes -> mapM_ print filmes
