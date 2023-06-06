3c7b79941cd92f2ae0223d5bfa451b10e1404f4e
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-06-06 18:11:23 -0300

    Pesquisa

--- logico/main.pl
+++ logico/main.pl
@@ -1,24 +1,22 @@
 :- consult('filme.pl').
-
+:- consult('pesquisa.pl').
 
 main :-
     adicionarFilmePorParametros('O Poderoso Chefão', ['Crime', 'Drama'], 'Um chefão da máfia luta para manter seu império.', 'Francis Ford Coppola', ['Marlon Brando', 'Al Pacino', 'James Caan'], 1972, 175, 9.2, 9.1),
-    adicionarFilmePorParametros('Interestelar', ['Ficção científica', 'Drama'], 'Um grupo de astronautas viaja através de um buraco de minhoca em busca de um novo planeta para habitar.', 'Christopher Nolan', ['Matthew McConaughey', 'Anne Hathaway', 'Jessica Chastain'], 2014, 169, 8.6, 8.5),
+    adicionarFilmePorParametros('Interestelar', ['Ficção científica', 'Drama'], 'Um grupo de astronautas viaja através de um buraco de minhoca em busca de um novo planeta para habitar.', 'Christopher Nolan', ['Matthew McConaughey', 'Anne Hathaway', 'Jessica Chastain'], 2002, 169, 8.6, 8.5),
     adicionarFilmePorParametros('Matrix', ['Ação', 'Ficção científica'], 'Um hacker descobre que a realidade em que vive é uma simulação criada por máquinas.', 'Lana Wachowski', ['Keanu Reeves', 'Laurence Fishburne', 'Carrie-Anne Moss'], 1999, 136, 8.7, 8.8),
-    adicionarFilmePorParametros('O Senhor dos Anéis: A Sociedade do Anel', ['Aventura', 'Fantasia'], 'Um grupo de heróis se une para destruir um poderoso anel e salvar a Terra Média.', 'Peter Jackson', ['Elijah Wood', 'Ian McKellen', 'Viggo Mortensen'], 2001, 178, 8.8, 8.7).
-    adicionarFilmePorParametros('A Origem', ['Ação', 'Ficção científica'], 'Um grupo de ladrões especializados invade os sonhos das pessoas para extrair informações valiosas.', 'Christopher Nolan', ['Leonardo DiCaprio', 'Joseph Gordon-Levitt', 'Elliot Page'], 2010, 148, 8.8, 8.7),
-    adicionarFilmePorParametros('Pulp Fiction: Tempo de Violência', ['Crime', 'Drama'], 'Histórias interligadas retratando o submundo do crime em Los Angeles.', 'Quentin Tarantino', ['John Travolta', 'Samuel L. Jackson', 'Uma Thurman'], 1994, 154, 8.9, 8.8),
-    adicionarFilmePorParametros('Clube da Luta', ['Drama'], 'Um homem descontente cria um clube de luta clandestino como forma de escape da sua vida monótona.', 'David Fincher', ['Brad Pitt', 'Edward Norton', 'Helena Bonham Carter'], 1999, 139, 8.8, 8.7),
-    adicionarFilmePorParametros('O Labirinto do Fauno', ['Fantasia', 'Drama'], 'Uma garota escapa para um mundo fantástico durante a Guerra Civil Espanhola.', 'Guillermo del Toro', ['Ivana Baquero', 'Sergi López', 'Maribel Verdú'], 2006, 118, 8.2, 8.4),
+    adicionarFilmePorParametros('O Senhor dos Anéis: A Sociedade do Anel', ['Aventura', 'Fantasia'], 'Um grupo de heróis se une para destruir um poderoso anel e salvar a Terra Média.', 'Peter Jackson', ['Elijah Wood', 'Ian McKellen', 'Viggo Mortensen'], 2002, 178, 8.8, 8.7),
+    adicionarFilmePorParametros('A Origem', ['Ação', 'Ficção científica'], 'Um grupo de ladrões especializados invade os sonhos das pessoas para extrair informações valiosas.', 'Hayao Miyazaki', ['Leonardo DiCaprio', 'Joseph Gordon-Levitt', 'Elliot Page'], 2010, 148, 8.8, 8.7),
+    adicionarFilmePorParametros('Pulp Fiction', ['Crime', 'Drama'], 'Histórias interligadas retratando o submundo do crime em Los Angeles.', 'Quentin Tarantino', ['John Travolta', 'Samuel L. Jackson', 'Uma Thurman'], 1994, 154, 8.9, 8.8),
+    adicionarFilmePorParametros('A Origem', ['Drama'], 'Um homem descontente cria um clube de luta clandestino como forma de escape da sua vida monótona.', 'Hayao Miyazaki', ['Brad Pitt', 'Edward Norton', 'Helena Bonham Carter'], 1999, 139, 8.8, 8.7),
+    adicionarFilmePorParametros('O Labirinto do Fauno', ['Fantasia', 'Drama'], 'Uma garota escapa para um mundo fantástico durante a Guerra Civil Espanhola.', 'Guillermo del Toro', ['Ivana Baquero', 'Sergi López', 'Leandro Firmino', 'Maribel Verdú'], 2006, 118, 8.2, 8.4),
     adicionarFilmePorParametros('Cidade de Deus', ['Crime', 'Drama'], 'A história de diversos personagens envolvidos no crime organizado em uma favela do Rio de Janeiro.', 'Fernando Meirelles', ['Alexandre Rodrigues', 'Leandro Firmino', 'Phellipe Haagensen'], 2002, 130, 8.6, 8.5),
     adicionarFilmePorParametros('A Viagem de Chihiro', ['Animação', 'Aventura'], 'Uma menina se aventura em um mundo mágico para resgatar seus pais transformados em porcos.', 'Hayao Miyazaki', ['Rumi Hiiragi', 'Miyu Irino', 'Mari Natsuki'], 2001, 125, 8.6, 8.5),
-    recuperarTodosFilmes(Filmes),
-    percorrerFilmes(Filmes),
-    write("Ok").
+    selecaoPesquisa(Resultado),
+    percorrerFilmes(Resultado),
+    writeln("Ok").
 
 percorrerFilmes([]).
-percorrerFilmes([Filme | Resto]) :-
-    % getAtributos(Filme,Titulo),
-    % writeln(Titulo),
+percorrerFilmes([Filme|Resto]) :-
     mostrarFilme(Filme),
     percorrerFilmes(Resto).
