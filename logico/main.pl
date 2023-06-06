8d1a4acda022cd5e6d0629b4cb2d3b34608b6444
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-06-06 18:07:42 -0300

    Pesquisa

--- logico/main.pl
+++ logico/main.pl
@@ -2,8 +2,16 @@
 
 
 main :-
-    adicionarFilme,
-    adicionarFilme,
+    adicionarFilmePorParametros('O Poderoso Chefão', ['Crime', 'Drama'], 'Um chefão da máfia luta para manter seu império.', 'Francis Ford Coppola', ['Marlon Brando', 'Al Pacino', 'James Caan'], 1972, 175, 9.2, 9.1),
+    adicionarFilmePorParametros('Interestelar', ['Ficção científica', 'Drama'], 'Um grupo de astronautas viaja através de um buraco de minhoca em busca de um novo planeta para habitar.', 'Christopher Nolan', ['Matthew McConaughey', 'Anne Hathaway', 'Jessica Chastain'], 2014, 169, 8.6, 8.5),
+    adicionarFilmePorParametros('Matrix', ['Ação', 'Ficção científica'], 'Um hacker descobre que a realidade em que vive é uma simulação criada por máquinas.', 'Lana Wachowski', ['Keanu Reeves', 'Laurence Fishburne', 'Carrie-Anne Moss'], 1999, 136, 8.7, 8.8),
+    adicionarFilmePorParametros('O Senhor dos Anéis: A Sociedade do Anel', ['Aventura', 'Fantasia'], 'Um grupo de heróis se une para destruir um poderoso anel e salvar a Terra Média.', 'Peter Jackson', ['Elijah Wood', 'Ian McKellen', 'Viggo Mortensen'], 2001, 178, 8.8, 8.7).
+    adicionarFilmePorParametros('A Origem', ['Ação', 'Ficção científica'], 'Um grupo de ladrões especializados invade os sonhos das pessoas para extrair informações valiosas.', 'Christopher Nolan', ['Leonardo DiCaprio', 'Joseph Gordon-Levitt', 'Elliot Page'], 2010, 148, 8.8, 8.7),
+    adicionarFilmePorParametros('Pulp Fiction: Tempo de Violência', ['Crime', 'Drama'], 'Histórias interligadas retratando o submundo do crime em Los Angeles.', 'Quentin Tarantino', ['John Travolta', 'Samuel L. Jackson', 'Uma Thurman'], 1994, 154, 8.9, 8.8),
+    adicionarFilmePorParametros('Clube da Luta', ['Drama'], 'Um homem descontente cria um clube de luta clandestino como forma de escape da sua vida monótona.', 'David Fincher', ['Brad Pitt', 'Edward Norton', 'Helena Bonham Carter'], 1999, 139, 8.8, 8.7),
+    adicionarFilmePorParametros('O Labirinto do Fauno', ['Fantasia', 'Drama'], 'Uma garota escapa para um mundo fantástico durante a Guerra Civil Espanhola.', 'Guillermo del Toro', ['Ivana Baquero', 'Sergi López', 'Maribel Verdú'], 2006, 118, 8.2, 8.4),
+    adicionarFilmePorParametros('Cidade de Deus', ['Crime', 'Drama'], 'A história de diversos personagens envolvidos no crime organizado em uma favela do Rio de Janeiro.', 'Fernando Meirelles', ['Alexandre Rodrigues', 'Leandro Firmino', 'Phellipe Haagensen'], 2002, 130, 8.6, 8.5),
+    adicionarFilmePorParametros('A Viagem de Chihiro', ['Animação', 'Aventura'], 'Uma menina se aventura em um mundo mágico para resgatar seus pais transformados em porcos.', 'Hayao Miyazaki', ['Rumi Hiiragi', 'Miyu Irino', 'Mari Natsuki'], 2001, 125, 8.6, 8.5),
     recuperarTodosFilmes(Filmes),
     percorrerFilmes(Filmes),
     write("Ok").
--- logico/pesquisa.pl
+++ logico/pesquisa.pl
@@ -1,23 +1,103 @@
-% Método usado para pesquisar determinado filme pelo seu diretor
-pesquisaPorDiretor(Filmes, Diretor, Resultado) :-
-    include(mesmoDiretor(Diretor), Filmes, Resultado),
-    Resultado \= [].
+% Método usado para pesquisar determinado filme por seu nome
+pesquisarFilmesPorTitulo(Titulo, Filmes) :-
+    findall(
+        filme{
+            titulo: Titulo,
+            generos: Generos,
+            descricao: Descricao,
+            diretor: Diretor,
+            atores: Atores,
+            dataLancamento: DataLancamento,
+            duracao: Duracao,
+            notaImdb: NotaImdb,
+            notaUsuario: NotaUsuario
+        },
+        filme(Titulo, Generos, Descricao, Diretor, Atores, DataLancamento, Duracao, NotaImdb, NotaUsuario),
+        Filmes
+    ).
 
-mesmoDiretor(Diretor, Filme) :-
-    getDiretorFilme(Filme, Diretor).
+% Método usado para pesquisar determinado filme por seu diretor
+pesquisarFilmesPorDiretor(Diretor, Filmes) :-
+    findall(
+        filme{
+            titulo: Titulo,
+            generos: Generos,
+            descricao: Descricao,
+            diretor: Diretor,
+            atores: Atores,
+            dataLancamento: DataLancamento,
+            duracao: Duracao,
+            notaImdb: NotaImdb,
+            notaUsuario: NotaUsuario
+        },
+        filme(Titulo, Generos, Descricao, Diretor, Atores, DataLancamento, Duracao, NotaImdb, NotaUsuario),
+        Filmes
+    ).
+
+% Método usado para pesquisar determinado filme por sua data de lancamento
+pesquisarFilmesPorDataLancamento(DataLancamento, Filmes) :-
+    findall(
+        filme{
+            titulo: Titulo,
+            generos: Generos,
+            descricao: Descricao,
+            diretor: Diretor,
+            atores: Atores,
+            dataLancamento: DataLancamento,
+            duracao: Duracao,
+            notaImdb: NotaImdb,
+            notaUsuario: NotaUsuario
+        },
+        filme(Titulo, Generos, Descricao, Diretor, Atores, DataLancamento, Duracao, NotaImdb, NotaUsuario),
+        Filmes
+    ).
 
 % Método usado para pesquisar determinado filme por um de seus atores
-pesquisaPorAtor(Filmes, Ator, Resultado) :-
-    include(mesmoAtor(Ator), Filmes, Resultado),
-    Resultado \= [].
+pesquisarFilmesPorAtor(Ator, Filmes) :-
+    findall(
+        filme{
+            titulo: Titulo,
+            generos: Generos,
+            descricao: Descricao,
+            diretor: Diretor,
+            atores: Atores,
+            dataLancamento: DataLancamento,
+            duracao: Duracao,
+            notaImdb: NotaImdb,
+            notaUsuario: NotaUsuario
+        },
+        (
+            filme(Titulo, Generos, Descricao, Diretor, Atores, DataLancamento, Duracao, NotaImdb, NotaUsuario),
+            member(Ator, Atores)
+        ),
+        Filmes
+    ).
 
-mesmoAtor(Ator, Filme) :-
-    getAtoresFilme(Filme, Atores),
-    member(Ator, Atores).
+% Método usado para pesquisar determinado filme por um de seus gêneros
+pesquisarFilmesPorGenero(Genero, Filmes) :-
+    findall(
+        filme{
+            titulo: Titulo,
+            generos: Generos,
+            descricao: Descricao,
+            diretor: Diretor,
+            atores: Atores,
+            dataLancamento: DataLancamento,
+            duracao: Duracao,
+            notaImdb: NotaImdb,
+            notaUsuario: NotaUsuario
+        },
+        (
+            filme(Titulo, Generos, Descricao, Diretor, Atores, DataLancamento, Duracao, NotaImdb, NotaUsuario),
+            member(Genero, Generos)
+        ),
+        Filmes
+    ).
 
-% Método usado para adequar a pesquisa de acordo com o quê o usuário quer; funciona como
-% algumas funcionalidades do main e trata exceções, como opções inválidas
-selecaoPesquisa(Rep) :-
+
+
+
+selecaoPesquisa(Filmes) :-
     writeln('Digite o número de acordo com a pesquisa que deseja fazer:'),
     writeln('1) Nome'),
     writeln('2) Gênero'),
@@ -26,57 +106,27 @@ selecaoPesquisa(Rep) :-
     writeln('5) Ator'),
     writeln('Qual sua escolha: '),
     read_line_to_string(user_input, Cmd),
-    filmes(Rep, Filmes),
     (
         Cmd = "1" ->
             writeln('Digite o nome do filme:'),
             read_line_to_string(user_input, Nome),
-            pesquisaPorNome(Filmes, Nome, Resultado),
-            (
-                Resultado = [] ->
-                    writeln('Nenhum filme encontrado.')
-                ;
-                    maplist(writeln, Resultado)
-            )
+            pesquisarFilmesPorTitulo(Nome, Filmes)
         ; Cmd = "2" ->
             writeln('Digite o gênero:'),
             read_line_to_string(user_input, Genero),
-            pesquisaPorGenero(Filmes, Genero, Resultado),
-            (
-                Resultado = [] ->
-                    writeln('Nenhum filme encontrado.')
-                ;
-                    maplist(writeln, Resultado)
-            )
+            pesquisarFilmesPorGenero(Genero, Filmes)
         ; Cmd = "3" ->
             writeln('Digite o ano:'),
             read_line_to_string(user_input, Ano),
-            pesquisaPorAno(Filmes, Ano, Resultado),
-            (
-                Resultado = [] ->
-                    writeln('Nenhum filme encontrado.')
-                ;
-                    maplist(writeln, Resultado)
-            )
+            pesquisarFilmesPorDataLancamento(Ano, Filmes)
         ; Cmd = "4" ->
             writeln('Digite o Diretor:'),
             read_line_to_string(user_input, Diretor),
-            pesquisaPorDiretor(Filmes, Diretor, Resultado),
-            (
-                Resultado = [] ->
-                    writeln('Nenhum filme encontrado.')
-                ;
-                    maplist(writeln, Resultado)
-            )
+            pesquisarFilmesPorDiretor(Diretor, Filmes)
         ; Cmd = "5" ->
             writeln('Digite o Ator:'),
             read_line_to_string(user_input, Ator),
-            pesquisaPorAtor(Filmes, Ator, Resultado),
-            (
-                Resultado = [] ->
-                    writeln('Nenhum filme encontrado.')
-                ;
-                    maplist(writeln, Resultado)
-            )
-        ; writeln('Opção inválida.')
+            pesquisarFilmesPorAtor(Ator, Filmes)
+        ; writeln('Opção inválida.'),
+          fail % falha para sair do predicado se a opção for inválida
     ).
